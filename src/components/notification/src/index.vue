a55ba9f22a2ac60fed88b80076c26c30015f3794
NtoStudy <2943343011@qq.com>
2025-03-05 20:45:14 +0800

    通知类型组件封装

---
+++ src/components/notification/src/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const props = defineProps({
+  icon: {
+    type: String,
+    default: 'Bell'
+  },
+  value: {
+    type: [String, Number],
+    default: ''
+  },
+  max: {
+    type: Number,
+    default: 0
+  },
+  isDot: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <el-popover
+      placement="bottom"
+      :width="300"
+      trigger="click"
+  >
+    <template #default>
+      <slot></slot>
+    </template>
+    <template #reference>
+      <el-badge :value="value" :max="max" :is-dot="isDot">
+        <el-icon>
+          <component :is="icon"></component>
+        </el-icon>
+      </el-badge>
+    </template>
+  </el-popover>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
